b863f4153ceccf3d7cdd6496c6f4de3b8d625f20
code-logik <mark.sarasua@code-logik.com>
2024-04-24 17:08:03 -0500

    Create PSPAPI.cs

---
+++ src/TheWillowTreeOMS/CustomerUserInterface/PSPAPI.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * Mark Sarasua
+ * www.code-logik.com
+ * KMSAPI.cs
+ * 03 APRIL 2024
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace OMS
+{
+    /// <summary>
+    /// Class <c>PSPAPI</c> is a stub class for Payment Service Provider calls.
+    /// </summary>
+    internal class PSPAPI
+    {
+        /// <summary>
+        /// Process order.
+        /// </summary>
+        /// <param name="order">Order to process.</param>
+        public void ProcessOrder(List<object[]> order) 
+        {
+            // stub
+            foreach (object[] item in order) 
+            {
+                if (item.Length > 1) 
+                {
+                    foreach(object element in item) 
+                    {
+                        Console.Write($" {element} ");
+                    }
+
+                    Console.WriteLine();
+                }
+                else 
+                {
+                    Console.WriteLine($"{item[0]}");
+                }
+            }
+        }
+    }
+}
